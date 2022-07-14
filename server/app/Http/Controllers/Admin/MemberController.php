5565e166fea27ad7d54b40c7f3cf22d62061df03
phi hung <hungn12333@gmail.com>
2022-07-14 15:23:31 +0700

    Update Authen

--- server/app/Http/Controllers/Admin/MemberController.php
+++ server/app/Http/Controllers/Admin/MemberController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use app\models\customer;
+use app\models\Users;
 use App\Http\Requests\Member\StoreRequest;
 use App\Http\Requests\Member\UpdateRequest;
 
@@ -15,12 +15,8 @@ use App\Http\Requests\Member\UpdateRequest;
 class MemberController extends Controller
 {
     public function index(){
-<<<<<<< HEAD
-        $result = DB::table('users')->orderBy('created_at', 'DESC')->get();
-=======
         // $result = DB::table('customers')->orderBy('created_at', 'DESC')->get();
-        $result=customer::orderBy('created_at','ASC')->get();
->>>>>>> 458a5d1bb58ea89d6bb09c0c763027f0a49369b0
+        $result=Users::orderBy('created_at','ASC')->get();
         return view('admin.pages.member.index', ['members' => $result]);
 
     }
@@ -33,34 +29,15 @@ class MemberController extends Controller
         $data = $request->except('_token');
         $data['password'] = bcrypt($request->password);
         $data['created_at'] = new \DateTime();
-<<<<<<< HEAD
-        DB::table('users')->insert($data);
-
-=======
-        $obj = new customer();
+        $obj = new Users();
         $obj -> fill($data);
         $obj->save();
         // DB::table('customers')->insert($data);
->>>>>>> 458a5d1bb58ea89d6bb09c0c763027f0a49369b0
         return redirect()->route('admin.member.index')->with('success', 'Insert successfully');
     }
 
     public function edit($id){
-<<<<<<< HEAD
-        $member = DB::table('users')->where('customer_id', $id)->first();
-        $edit_myself=null;
-        // dd(Auth::User());
-        // if(Auth::user()->customer_id == $id){
-        //     $edit_myself=true;
-        // } else{
-        //     $edit_myself = false;
-        // }
-        // dd('asd');
-        // if(Auth::user()->customer_id != 1 && ($id == 1 || ($member->level == 1 && $edit_myself == false))){
-        //     return "khong sua duoc";
-        // }
-=======
-        $member = DB::table('customers')->where('customer_id', $id)->first();
+        $member = DB::table('users')->where('user_id', $id)->first();
         // $edit_myself=null;
         // dd($member->level); 
         if(Auth::user()->customer_id == $id){
@@ -69,7 +46,6 @@ class MemberController extends Controller
             $edit_myself = false;
         }
         
->>>>>>> 458a5d1bb58ea89d6bb09c0c763027f0a49369b0
         return view('admin.pages.member.edit', ['id'=>$id, 'member' => $member])->with('success', 'Edit successfully');
     }
 
@@ -80,7 +56,7 @@ class MemberController extends Controller
         if(!empty($request->password)){
             $data['password'] = bcrypt($request->password);
         }
-        customer::where('customer_id', $id)->update($data);
+        Users::where('customer_id', $id)->update($data);
         // dd(customer::find($id)->update($data));
         
         
@@ -88,7 +64,7 @@ class MemberController extends Controller
     }
     
     public function delete($id){
-        $result = DB::table('customers')->where('customer_id', '=', $id)->delete();
+        $result = DB::table('users')->where('user_id', '=', $id)->delete();
         return redirect()->route('admin.member.index');
     }
 }
--- server/resources/views/admin/pages/member/edit.blade.php
+++ server/resources/views/admin/pages/member/edit.blade.php
@@ -30,10 +30,7 @@
                                         <form class="user" action="{{route('admin.member.update', ['id' => $id])}}" method="POST">
                                             @csrf
                                             <div class="form-group row" >
-                                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                                    <input type="text" name="username" class="form-control form-control-user" id="exampleFirstName"
-                                                        placeholder="Username" value={{$member->username}}>
-                                                </div>
+                                                
                                                 <div class="col-sm-6">
                                                     <input type="text" name="fullname" class="form-control form-control-user" id="exampleLastName"
                                                         placeholder="Your Full Name" value={{$member->fullname}}>
